52a6416cb8f2f16cc9377629b6da1b7249d468dc
Karissa DeLaunay <karissadelaunay@Karissas-MacBook-Air.local>
2021-12-11 13:55:26 -0800

    url lookup js

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+const smLookup = {
+    heart: {
+        imageUrl: 'https://i.imgur.com/y6Ke2h1.jpg',
+    },
+    cry: {
+        imageUrl: 'https://i.imgur.com/WHwjsY6.jpg',
+    },
+    crazy: {
+        imageUrl: 'https://i.imgur.com/T7sxx5o.jpg',
+    },
+};
