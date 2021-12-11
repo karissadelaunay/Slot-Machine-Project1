146e6ac5bc026cdcf5a97e475c213b9a09c30f54
Karissa DeLaunay <karissadelaunay@Karissas-MacBook-Air.local>
2021-12-11 14:08:13 -0800

    state variables

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,22 @@ const smLookup = {
         imageUrl: 'https://i.imgur.com/T7sxx5o.jpg',
     },
 };
+
+//state variables(use an object)
+let wins;
+
+let losses;
+
+let choices;
+
+//initial value 
+//{
+// wins: 0,
+//losses: 0,
+//}
+
+//{
+//choiceOne: 'heart , 'cry', 'crazy',
+//choiceTwo: 'heart , 'cry', 'crazy',
+//choiceThree: 'heart , 'cry', 'crazy',  
+//}
